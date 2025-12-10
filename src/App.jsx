62978b529ed2a044a06908055c258f83c1dc45cf
khadija-tannahi <khadija.tannahi@gmail.com>
2025-12-09 20:38:44 -0500

    added TodoListItem component and newTodo state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'; // Importing useState hook from React
 import './App.css';
 import TodoList from './TodoList'; // Importing TodoList component
 import TodoForm from './TodoForm'; // Importing TodoForm component
 
 function App() {
+  const [newTodo, setNewTodo] = useState('example todo'); // State to manage new todo input
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
