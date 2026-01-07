a14b093039f294f49d5ed1d9a76928bb9a74d3bc
khadija-tannahi <khadija.tannahi@gmail.com>
2026-01-06 19:07:59 -0500

    added edit todo functionality with TextInputWithLabel component

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,26 @@ function App() {
     setTodoList(updatedTodos);
   }
 
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  }
+
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo} />
 
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </div>
   );
 }
